88e2fab9d33c4ebc7ac4508108dee6fa5a43ea70
lincidro <lincidro@gmail.com>
2020-01-03 00:20:54 -0500

    Se agrega crossorigin para realizar llamadas en angular-ecommerce

--- src/main/java/com/example/demo/dao/ProductRepository.java
+++ src/main/java/com/example/demo/dao/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.demo.entity.Product;
 
+//@CrossOrigin({"http://localhost:4200","http://localhost:4100"})
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
